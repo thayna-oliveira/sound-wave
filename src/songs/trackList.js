b25a445d88d4c4d546ae8182936a23efc868f049
thayna-oliveira <oliveira.thayna@outlook.com>
2023-08-06 16:33:42 +0100

    add tracklist data

---
+++ src/songs/trackList.js
@@ -0,0 +1,27 @@
+import falconerMp3 from "./Falconer - Southern Roots Boogie.mp3";
+import falconerCover from "./falconer.png";
+import saxMp3 from "./Ofer Koren - Sax Party.mp3";
+import rawMp3 from "./Raw - Nonsense.mp3";
+import rawCover from "./raw.png";
+import saxCover from "./sax.png";
+
+export const trackList = [
+  {
+    title: "Nonsense",
+    author: "Raw",
+    src: rawMp3,
+    thumbnail: rawCover,
+  },
+  {
+    title: "Sax Party",
+    author: "Ofer Koren",
+    src: saxMp3,
+    thumbnail: saxCover,
+  },
+  {
+    title: "Southern Roots Boogie",
+    author: "Falconer",
+    src: falconerMp3,
+    thumbnail: falconerCover,
+  },
+];
