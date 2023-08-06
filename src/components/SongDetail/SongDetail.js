1a3b582be2c20069be5fe7aafa439b1226314483
thayna-oliveira <oliveira.thayna@outlook.com>
2023-08-06 16:22:11 +0100

    add song detail component

---
+++ src/components/SongDetail/SongDetail.js
@@ -0,0 +1,28 @@
+import styled from "styled-components";
+import {colors} from "../../styles";
+
+const Wrapper = styled.div`
+  text-align: left;
+  font-size: 18px;
+`;
+
+const Title = styled.h3`
+  color: ${colors.light12};
+  font-weight: 700;
+`;
+
+const Author = styled.p`
+  color: ${colors.light10};
+  font-weight: 400;
+`;
+
+const SongDetail = ({songName, author}) => {
+  return (
+    <Wrapper>
+      <Title>{songName}</Title>
+      <Author>{author}</Author>
+    </Wrapper>
+  );
+};
+
+export default SongDetail;
---
+++ src/components/index.js
@@ -0,0 +1,1 @@
+export {default as SongDetail} from "./SongDetail";
